94e8e13c29360fe8085e936e1f9f49babd5f9d98
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2023-07-06 12:54:21 +0600

    file splitted

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const port = 5000;
+import app from "./app";
+import mongoose from "mongoose";
+
+const port: number = 5000;
 
 // database connection
 async function bootstrap() {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/mongoose-practice");
     console.log(`📦 Database connection successful`);
+
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
   } catch (err) {
     console.error(`❌ Failed to connect database`, err);
   }
 }
 bootstrap();
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
