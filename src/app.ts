6566d00f831844a045558e9c5820ffc4cfffd2cf
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2023-07-09 16:10:32 +0600

    interface and schema comparison

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,76 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 
+import { Schema } from "mongoose";
 import cors from "cors";
 
 const app: Application = express();
+
+// Using cors
 app.use(cors());
 
+// parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World!");
-  next();
+  // inserting a test data into mongodb
+  /*
+
+  * Step 1: Interface
+  * Step 2: Schema
+  * Step 3 : Model
+  * Step 4 : Database Query
+
+  */
+
+  // creating an interface
+  interface IUser {
+    id: string;
+    role: "student";
+    password: string;
+    name: {
+      firstName: string;
+      middleName?: string;
+      lastName: string;
+    };
+    dateOfBirth?: string;
+    gender: "male" | "female";
+    email?: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    presentAddress: string;
+    permanentAddress: string;
+  }
+
+  // creating Schema using interface
+  const userSchema = new Schema<IUser>({
+    id: { type: String, required: true, unique: true },
+    role: { type: String, required: true },
+    password: { type: String, required: true },
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      middleName: {
+        type: String,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    dateOfBirth: { type: String },
+    gender: { type: String, enum: ["male", "female"] },
+    email: { type: String },
+    contactNo: { type: String, required: true },
+    emergencyContactNo: { type: String, required: true },
+    presentAddress: { type: String, required: true },
+    permanentAddress: { type: String, required: true },
+  });
+
+  //   res.send("Hello World!");
+  //   next();
 });
 
 export default app;
